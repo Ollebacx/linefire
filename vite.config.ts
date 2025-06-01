05c65461483201b0993374767cbc0e01278ef120
David Cabello Oquendo <davidcoq@inditex.com>
2025-06-01 20:11:00 +0200

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
     return {
+      plugins: [react()],
+      base: '/linefire/', // 👈 muy importante para GitHub Pages
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
